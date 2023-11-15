5eecd0c9017d7309d62f097156f29c041c9294c6
Studente <Studente@10.1.30.252>
2023-11-15 12:55:33 +0100

    Correzione semantica

--- src/addizioni.java
+++ src/addizioni.java
@@ -4,10 +4,10 @@ public class addizioni {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("A: ");	int a = sc.nextInt();
-		System.out.println("B: ");	int b = sc.nextInt();
+		System.out.print("A: ");	int a = sc.nextInt();
+		System.out.print("B: ");	int b = sc.nextInt();
 		
-		System.out.println("La somma é " + a + a);
+		System.out.println("La somma é " + (a + a));
 		
 		sc.close();
 	}
