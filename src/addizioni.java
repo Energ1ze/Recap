9507439be02d4e700284cb7f736a4ca8fc64503f
Studente <Studente@10.1.30.252>
2023-11-15 12:41:32 +0100

    Messaggio iniziale

---
+++ src/addizioni.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class addizioni {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("A: ");	int a = sc.nextInt();
+		System.out.println("B: ");	int b = sc.nextInt();
+		
+		System.out.println("La somma é " + a + a);
+		
+		sc.close();
+	}
+
+}
